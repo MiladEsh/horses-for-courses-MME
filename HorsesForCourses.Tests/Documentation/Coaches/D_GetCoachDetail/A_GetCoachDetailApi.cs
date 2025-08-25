90ff0a716ffd3971708c42457dc79cd45d8cfc42
kilfour <kilfour@gmail.com>
2025-08-25 21:54:35 +0200

    another test

---
+++ HorsesForCourses.Tests/Documentation/Coaches/D_GetCoachDetail/A_GetCoachDetailApi.cs
@@ -0,0 +1,31 @@
+using HorsesForCourses.Api.Coaches;
+using HorsesForCourses.Api.Coaches.GetCoaches;
+using HorsesForCourses.Api.Warehouse.Paging;
+using HorsesForCourses.Tests.Tools.Coaches;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace HorsesForCourses.Tests.Documentation.Coaches.D_GetCoachDetail;
+
+public class A_GetCoachDetailApi : CoachesControllerTests
+{
+    private async Task<OkObjectResult?> Act()
+    {
+        return await controller.GetCoachDetail(42) as OkObjectResult;
+    }
+
+    [Fact]
+    public async Task GetCoachDetail_uses_the_query_object()
+    {
+        var result = await Act();
+        getCoachDetail.Verify(a => a.One(It.IsAny<int>()));
+    }
+
+    [Fact]
+    public async Task GetCoachDetailReturnsOk_With_List()
+    {
+        var result = await Act();
+        Assert.NotNull(result);
+        Assert.IsType<CoachDetail>(result!.Value);
+    }
+}
---
+++ HorsesForCourses.Tests/Documentation/Coaches/D_GetCoachDetail/B_GetCoachDetailData.cs
@@ -0,0 +1,23 @@
+using HorsesForCourses.Api.Coaches.GetCoaches;
+using HorsesForCourses.Tests.Tools;
+
+
+namespace HorsesForCourses.Tests.Documentation.Coaches.D_GetCoachDetail;
+
+
+public class B_GetCoachDetailData : TheDatabaseTest
+{
+    private async Task<CoachDetail?> Act()
+        => await new GetCoachDetail(GetDbContext()).One(1);
+
+    [Fact]
+    public async Task With_Coach()
+    {
+        AddToDb(TheCannonical.Coach());
+        var detail = await Act();
+        Assert.NotNull(detail);
+        Assert.Equal(1, detail.Id);
+        Assert.Equal(TheCannonical.CoachName, detail.Name);
+        Assert.Equal(TheCannonical.CoachEmail, detail.Email);
+    }
+}
