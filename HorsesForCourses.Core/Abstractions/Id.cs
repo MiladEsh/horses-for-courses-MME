de1b63a7430068fa7a3a69f75cb22d1dd7f81b12
kilfour <kilfour@gmail.com>
2025-08-26 11:24:59 +0200

    removed cruft

--- HorsesForCourses.Core/Abstractions/Id.cs
+++ HorsesForCourses.Core/Abstractions/Id.cs
@@ -11,10 +11,6 @@ public record Id<T>
 
     private Id(int value) => this.value = value;
 
-    //public override string ToString() => $"{typeof(T).Name}: {value}";
-
-    //public static Id<T> New() => new Id<T>(Guid.NewGuid());
-
     public static Id<T> From(int value) => new(value);
 }
 
